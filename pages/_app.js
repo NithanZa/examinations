78f38a8aec4db4c950386cc3fa502b255024bb88
NithanZa <jaroonekp@gmail.com>
2025-05-02 17:36:59 +0700

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/react"
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <SessionProvider>
             <Component {...pageProps} />
+            <Analytics />
         </SessionProvider>
     );
 }
